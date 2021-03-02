2b0befce585ccc3257c2f61f48240b8c8d28ab8a
speeedforce7 <zstanislav98@gmail.com>
2021-03-02 21:44:21 +0200

    Second

---
+++ Writing/Essay/Untitled-2.bat
@@ -0,0 +1,15 @@
+Татьяна английский, [08.02.21 10:33]
+People can perform everyday tasks, such as shopping and banking as well as business transactions, without meeting other
+people face-to-face. What are the effects of this on individual and society as a whole?
+
+Татьяна английский, [08.02.21 10:33]
+Some people think visitors to others countries should imitate local customers and behaviours. Some people disagree; they
+think the host country should welcome cultural differences. Discuss the two views and give your opinion.
+
+Татьяна английский, [08.02.21 10:33]
+The advantages brought by the spread of English as a “global language” will outweigh the disadvantages. To what extent
+do you agree or disagree with this view?
+
+Татьяна английский, [08.02.21 10:34]
+Children who grow up in families which are short of money are better prepared with the problems of adult life than children
+who are brought up by wealthy parents. To what extent do you agree or disagree?
